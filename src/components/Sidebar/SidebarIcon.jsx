c48012c0f19b1acfd164af37de03fb634321a2b7
Kusen Abdullah <kusena322@gmail.com>
2023-06-06 13:30:42 +0700

    update component sidebar icon and update version

--- src/components/Sidebar/SidebarIcon.jsx
+++ src/components/Sidebar/SidebarIcon.jsx
@@ -6,9 +6,12 @@ const SidebarIconStyle = styled.a`
     margin-right: .2rem;
 `
 const SidebarIcon = (props) => {
-    return <SidebarIconStyle {...props}>
-        <Icon icon={props.icon} />
+    return <>
+        <SidebarIconStyle {...props}>
+            <Icon icon={props.icon} width={props?.width ? props.width : 30} height={props?.height ? props.height : 30} />
+        </SidebarIconStyle>
         {props?.title ? props.title : "default Menu"}
-    </SidebarIconStyle>
+    </>
+
 }
 export { SidebarIcon }
