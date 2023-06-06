945153e6a442b9e52e204b0ffcfffb3d484b89fd
Kusen Abdullah <kusena322@gmail.com>
2023-06-06 13:10:40 +0700

    import new component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { SidebarItem } from "./components/Sidebar/SidebarItem";
 import { SidebarIcon } from "./components/Sidebar/SidebarIcon";
 import { SidebarList } from "./components/Sidebar/SidebarList"
 import { Wrapper } from "./components/Wrapper/Wrapper";
+import {TopBar} from "./components/Content/TopBar"
+import {ToogleButton} from "./components/Content/ToogleButton"
 import * as styled from "styled-components"
 
 export {
-    Sidebar, SidebarHeading, Wrapper, styled, SidebarIcon, SidebarItem, SidebarList
+    Sidebar, SidebarHeading, Wrapper, styled, SidebarIcon, SidebarItem, SidebarList,TopBar,ToogleButton
 }
