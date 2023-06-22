e8562153bf9f00ea4516caefb858e05924cba495
Kusen Abdullah <kusena322@gmail.com>
2023-06-22 13:24:55 +0700

    #2 add bg color button

--- src/components/Button/FloatingButton.jsx
+++ src/components/Button/FloatingButton.jsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 import React from "react";
 import { Icons } from "../Icon/Icon";
-const FloatingButtonStyled = styled.a`
+const FloatingButtonStyled = styled.button`
     cursor: pointer;
     position: fixed;
     width: 3.7rem;
     height: 3.7rem;
     bottom: 2.5rem;
     right: 2.5rem;
-    background-color: #0d6efd;
+    background-color: ${props => props.bg ? props.bg : '#3485FD'};
     color: #FFF;
     border-radius: 50px;
     text-align: center;
--- src/components/Button/RoundedButton.jsx
+++ src/components/Button/RoundedButton.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import React from "react";
 import { Icons } from "../Icon/Icon";
 import { Link } from "react-router-dom";
-const RoundedButtonStyled = styled(Link)`
+const RoundedButtonStyledLink = styled(Link)`
     cursor: pointer;
     margin-right: .5rem;
     width: 2rem;
@@ -11,10 +11,28 @@ const RoundedButtonStyled = styled(Link)`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: #0C9;
+    background-color: ${props => props.bg ? props.bg : '#3485FD'};
 `;
+const RoundedButtonStyled = styled.button`
+    cursor: pointer;
+    margin-right: .5rem;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    outline:none;
+    border: 0;
+    background-color: ${props => props.bg ? props.bg : '#3485FD'};
+`
 const RoundedButton = (props) => {
-    return <RoundedButtonStyled to={props.go}>
+    if (props.type == "link") {
+        return <RoundedButtonStyledLink to={props.go} bg={props.bg}>
+            <Icons icon={props.icon} color={props.color} width={props?.width ? props.width : 20} height={props?.height ? props.height : 20} />
+        </RoundedButtonStyledLink>
+    }
+    return <RoundedButtonStyled bg={props.bg}>
         <Icons icon={props.icon} color={props.color} width={props?.width ? props.width : 20} height={props?.height ? props.height : 20} />
     </RoundedButtonStyled>
 }
